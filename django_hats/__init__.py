40f068274dc9725211787aa55f54292d0a6d85a9
Mike Hearing <mhearing@genepeeks.com>
2016-07-28 13:50:07 -0400

    Added basic role caching on the template
    Reduce hits to the database when data will not change on the template

--- django_hats/__init__.py
+++ django_hats/__init__.py
@@ -1,4 +1,4 @@
 __author__ = 'Michael Hearing'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 default_app_config = 'django_hats.apps.DjangoHatsConfig'
--- django_hats/context_processors.py
+++ django_hats/context_processors.py
@@ -4,10 +4,16 @@ from django_hats.roles import RoleFinder
 class RolesLookup(object):
     def __init__(self, user):
         self.user = user
+        self._cache = {}
 
-    def __getitem__(self, role):
-        role = RoleFinder.by_name(role)
-        return role.check_membership(self.user) if role is not None else False
+    def __getitem__(self, role_name):
+        # Check if the cache has our answer
+        if self._cache.get(role_name, None) is None:
+            role = RoleFinder.by_name(role_name)
+            self._cache[role.get_slug()] = role.check_membership(self.user) if role is not None else False
+
+        # Return the cached role
+        return self._cache[role_name]
 
     def __contains__(self, role):
         return self[role]
